3b11d94417be06c24b37caed73d07b1a569d44f7
Huy MG <imhuy112k@gmail.com>
2026-03-25 17:02:06 +0700

    commit 25-3

---
+++ Assets/2_Assets/0_Scrips/GamePlay/StaticDepthObject.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using Spine.Unity;
+using Spine;
+
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class StaticDepthObject : MonoBehaviour
+{
+    [Header("Settings")]
+    public float yOffset = 0f;
+
+    public float threshold = 0.05f;
+
+    private SpriteRenderer sr;
+    private Transform player;
+    //private SpriteRenderer playerSR;
+    private SkeletonAnimation playerSkeletonAnim;
+
+    private float lastPlayerY;
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        GameObject p = PlayerController.Instance != null ? PlayerController.Instance.gameObject : GameObject.FindGameObjectWithTag("Player").transform.GetChild(0).gameObject;
+
+        if (p != null)
+        {
+            player = p.transform;
+            //playerSR = p.GetComponent<SpriteRenderer>();
+            playerSkeletonAnim = p.GetComponent<SkeletonAnimation>();
+            lastPlayerY = player.position.y;
+        }
+        else
+        {
+            Debug.LogWarning("not found Player (tag = Player)");
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (player == null || playerSkeletonAnim == null) return;
+
+        float playerY = player.position.y;
+
+        if (Mathf.Abs(playerY - lastPlayerY) < 0.0001f) return;
+
+        lastPlayerY = playerY;
+
+        UpdateSorting(playerY);
+    }
+
+    void UpdateSorting(float playerY)
+    {
+        float myY = transform.position.y + yOffset;
+
+        if (myY < playerY - threshold)
+        {
+            //sr.sortingOrder = playerSR.sortingOrder + 1;
+            sr.sortingOrder = /*playerSkeletonAnim.GetComponent<Renderer>().sortingOrder + 10*/ 12;
+        }
+        else if (myY > playerY + threshold)
+        {
+            //sr.sortingOrder = playerSR.sortingOrder - 1;
+            sr.sortingOrder = /*playerSkeletonAnim.GetComponent<Renderer>().sortingOrder - 10*/ 4;
+        }
+    }
+}
